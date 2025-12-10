33074972accf395b8e5d5fa4381274c2335be483
ETM-00168 <ETM-00168@ETM-00168.home>
2025-12-10 13:26:31 -0300

    Conectando projeto com o Banco de Dados

---
+++ src/main/java/br/com/ifba/insfrastructure/entity/PesistenceEntity.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.ifba.insfrastructure.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ * @author ETM-00168
+ */
+@Getter
+@Setter
+@MappedSuperclass
+public class PesistenceEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    
+}
