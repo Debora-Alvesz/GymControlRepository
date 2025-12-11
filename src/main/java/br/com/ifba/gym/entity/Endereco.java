21035090851ba4d58541ef5b167fcdd5511a39dd
ETM-00168 <ETM-00168@ETM-00168.home>
2025-12-11 12:43:00 -0300

    Implementação da classe base Pessoa

---
+++ src/main/java/br/com/ifba/gym/entity/Endereco.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.ifba.gym.entity;
+
+import br.com.ifba.insfrastructure.entity.PesistenceEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ *
+ * @author ETM-00168
+ */
+@Getter //gera automaticamente os métodos get
+@Setter //gera automaticamente os métodos set
+@AllArgsConstructor // Gera um construtor que recebe todos os atributos
+@NoArgsConstructor // Gera um construtor vazio
+@Entity // será mapeada como uma tabela no banco de dados
+@Table(name = "Endereco") // Define o nome da tabela como "Endereco"
+public class Endereco extends PesistenceEntity{
+    
+    // Campo "cidade".
+    @Column(name = "Cidade", nullable = false)
+    private String cidade;
+    
+    // Campo "bairro".
+    @Column(name = "Bairro", nullable = false) 
+    private String bairro;
+    
+    // Campo "rua".
+    @Column(name = "Rua", nullable = false)
+    private String rua;
+    
+    // Campo "numero".
+    @Column(name = "Numero", nullable = false)
+    private int numero;
+    
+    // Campo "cep".
+    @Column(name = "Cep", nullable = false)
+    private String cep;
+    
+    // Campo "complemento".
+    @Column(name = "Complemento", nullable = false)
+    private String complemento;
+    
+}
---
+++ src/main/java/br/com/ifba/gym/entity/Pessoa.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.ifba.gym.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ *
+ * @author ETM-00168
+ */
+@Getter //gera automaticamente os métodos get
+@Setter //gera automaticamente os métodos set
+@Entity // Diz ao JPA que esta classe é uma tabela no banco de dados
+@AllArgsConstructor // Gera um construtor que recebe todos os atributos
+@NoArgsConstructor // Gera um construtor vazio
+@Table(name = "Pessoa") // Define o nome da tabela como "Pessoa"
+public class Pessoa{
+    
+    // Nome da pessoa, obrigatório (não pode ser nulo no banco)
+    @Column(name = "Nome", nullable = false)
+    private String nome;
+    
+    // CPF da pessoa, obrigatório e deve ser único no banco de dados
+    @Column(name = "CPF", nullable = false, unique = true)
+    @Id // diz que o atibuto é o primary key da tabela
+    private String cpf;
+    
+    // Telefone da pessoa, obrigatório
+    @Column(name = "Telefone", nullable = false)
+    private int telefone;
+    
+    // Data de nascimento da pessoa, nao necessariamente obrigatorio
+    @Column(name = "Nascimento")
+    private Date nascimento;
+    
+    // Email da pessoa, nao necessariamente obrigatorio
+    @Column(name = "Email")
+    private String email;
+    
+    // Relação 1:1 com Endereco (salva/atualiza junto com Pessoa)
+    @OneToOne(cascade = CascadeType.ALL)
+    // Cria a chave estrangeira "endereco_id" na tabela Pessoa
+    @JoinColumn(name = "endereco_id", nullable = false)
+    private Endereco endereco;
+    
+}
