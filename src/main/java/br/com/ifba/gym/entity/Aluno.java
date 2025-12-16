e54649d7b7ccb5e3d2ccc3c5f2a2b22c8f890889
João Victor de Souza Santana <20241ireads0004@ifba.edu.br>
2025-12-16 18:36:05 -0300

    SCRUM-12: Código da classe Aluno

--- src/main/java/br/com/ifba/gym/entity/Aluno.java
+++ src/main/java/br/com/ifba/gym/entity/Aluno.java
@@ -4,10 +4,43 @@
  */
 package br.com.ifba.gym.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 /**
  *
  * @author user
  */
-public class Aluno {
+//Gera metodos getters e setters
+@Getter
+@Setter
+//Gera construtores com todos argumentos e sem nenhum argumento
+@NoArgsConstructor
+@AllArgsConstructor
+//Cria os metodos Equals e HashCode
+@EqualsAndHashCode(callSuper = true)
+//Torna a classe "persistível" no BD
+@Entity
+//Altera o nome da tabela
+@Table(name = "alunos")
+//Define que a chave primaria de Aluno é a pk de Pessoa
+@PrimaryKeyJoinColumn(name = "pessoa_id")
+public class Aluno extends Pessoa{
     
+    @Column(name = "id_aluno", nullable = false, unique = true)
+    private int id_aluno;//Define o id do aluno como unico
+    
+    @Column(name = "matricula", nullable =  false, length = 20)
+    private String matricula;
+
+    @Column(name = "requisitos", nullable = false, length = 100)
+    private String requisitos; 
 }
