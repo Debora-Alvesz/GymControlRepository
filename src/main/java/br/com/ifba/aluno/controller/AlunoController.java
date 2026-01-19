02a3837ca9adfbe7b11e2b6d503052a930ce178c
Debora-Alvesz <20241ireads0021@ifba.edu.br>
2026-01-19 18:47:29 -0300

    Atualizando camadas Usuario e Aluno

--- src/main/java/br/com/ifba/aluno/controller/AlunoController.java
+++ src/main/java/br/com/ifba/aluno/controller/AlunoController.java
@@ -2,13 +2,10 @@
 package br.com.ifba.aluno.controller;
 
 import br.com.ifba.aluno.entity.Aluno;
-import br.com.ifba.aluno.service.AlunoService;
-import br.com.ifba.plano.entity.Plano;
-import br.com.ifba.plano.service.PlanoService;
+import br.com.ifba.aluno.service.AlunoIService;
 import java.util.List;
-import javax.swing.JOptionPane;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  *
@@ -16,49 +13,36 @@ import org.springframework.stereotype.Controller;
  */
 
 @Controller //diz para o spring que isso é um controller
-@RequiredArgsConstructor // o lombok cria o construtor nos campos com "final"
-public class AlunoController {
 
-    // Injetamos o Service de Aluno (para salvar o aluno)
-    private final AlunoService service;
+public class AlunoController implements AlunoIController {
     
-    // Injetamos o Service de Plano (para buscar a lista para o ComboBox)
-    private final PlanoService planoService;
-    
-    
-    //Busca os planos para preencher a tela (Requisito da Tarefa)
-    public List<Plano> buscarTodosPlanos() {
-        // Supondo que seu PlanoService tenha um método listarTodos ou findAll
-        return planoService.findAll();
+    @Autowired//O Spring injeta automaticamente a instância de ModalidadeService aqui
+    private AlunoIService alunoService;
+
+    @Override
+    public List<Aluno> findAll() {
+        return alunoService.findAll();
     }
-    //realiza a matricula
-    public void matricularAluno(String nome, String cpf, String email, String matricula, Plano plano) {
-       
-        try {
-            // Cria o objeto 
-            Aluno aluno = new Aluno();
-            
-            // Dados de Pessoa (Usando setNome em vez de setName, verifique sua entidade)
-            aluno.setName(nome); 
-            aluno.setCpf(cpf);
-            aluno.setEmail(email);
-            
-            // Dados de Aluno
-            aluno.setMatricula(matricula);
-            
-            // Associação do relacionamento 
-            aluno.setPlano(plano);
 
-            // Manda pro Service salvar
-            service.save(aluno); // Ajuste se o nome for 'save' ou 'salvar'
-
-            // Feedback de sucesso para o usuário
-            JOptionPane.showMessageDialog(null, "Matrícula realizada com sucesso!");
-
-        } catch (Exception e) {
-            // Tratamento de exceções
-            JOptionPane.showMessageDialog(null, "Falha ao realizar matrícula: " + e.getMessage());
-            e.printStackTrace(); 
-        }
+    @Override
+    public Aluno save(Aluno aluno) {
+        return alunoService.save(aluno);
     }
+
+    @Override
+    public Aluno update(String cpf, Aluno aluno) {
+        return alunoService.update(cpf, aluno);
+    }
+
+    @Override
+    public void delete(String cpf) {
+        alunoService.delete(cpf);
+    }
+
+    @Override
+    public Aluno findById(String cpf) {
+        // Se não encontrar, retorna null (padrão para telas Swing/Desktop)
+        return alunoService.findById(cpf).orElse(null);
+    }   
+ 
 }
