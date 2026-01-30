51b1d6fe2c99da7477384665a8da8d76b1a9c26a
João Victor de Souza Santana <20241ireads0004@ifba.edu.br>
2026-01-30 18:07:27 -0300

    SCRUM-71 Backend do Administrador

---
+++ src/main/java/br/com/ifba/administrador/service/AdministradorIService.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package br.com.ifba.administrador.service;
+
+import br.com.ifba.administrador.entity.Administrador;
+import java.util.List;
+
+/**
+ *
+ * @author João Victor
+ */
+public interface AdministradorIService {
+    
+    //Método para salvar Administrador
+    Administrador save(Administrador administrador);
+    
+    //Método para listar Administradores
+    List<Administrador> findAll();
+    
+    //Método para deletar Administrador
+    void delete(Administrador administrador);
+    
+    // Método específico para atualização
+    Administrador update(String cpf, Administrador administrador);
+}
